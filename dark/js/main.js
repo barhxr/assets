65ad2387dc707ea2d0bb71faf13605e3231985c0
BARHXR <43032720+barhxr@users.noreply.github.com>
2025-01-27 09:58:20 +0700

    Update main.js

--- dark/js/main.js
+++ dark/js/main.js
@@ -1,68 +1,91 @@
-(function ($) {
-    "use strict";
+(function (window, $) {
+	'use strict';
 
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
+	// Cache document for fast access.
+	var document = window.document;
+
+
+	/************** Toggle Menu *********************/
+	$('a.toggle-menu').click(function(){
+        $(".menu").slideToggle(400);
+		return false;
     });
 
 
-    // Sidebar Toggler
-    $('.sidebar-toggler').click(function () {
-        $('.sidebar, .content').toggleClass("open");
-        return false;
-    });
 
 
-    // Progress Bar
-    $('.pg-bar').waypoint(function () {
-        $('.progress .progress-bar').each(function () {
-            $(this).css("width", $(this).attr("aria-valuenow") + '%');
-        });
-    }, {offset: '80%'});
+    /************** Open Different Pages *********************/
+	$(".menu a").click(function(){
+		var id =  $(this).attr('class');
+		id = id.split('-');
+		$("#menu-container .content").hide();
+		$("#menu-container #menu-"+id[1]).addClass("animated fadeInDown").show();
+		return false;
+	});
+
+	$(".menu a.homebutton").click(function(){
+		$(".menu").slideUp();
+	});
 
 
-    // Calender
-    $('#calender').datetimepicker({
-        inline: true,
-        format: 'L'
-    });
+	$(window).resize(function(){
+		if ($(window).width() <= 769){	
+			$(".menu a").click(function(){
+				$(".menu").hide();
+				return false;
+			});
+		}	
+	});
+
+	/*
+	var dt = window.atob('IC0gPGEgcmVsPSJub2ZvbGxvdyIgaHJlZj0iaHR0cDovL3d3dy50ZW1wbGF0ZW1vLmNvbS9wcmV2aWV3L3RlbXBsYXRlbW9fNDEwX2NpcmNsZSI+Q2lyY2xlPC9hPiBieSA8YSByZWw9Im5vZm9sbG93IiBocmVmPSJodHRwOi8vd3d3LnRlbXBsYXRlbW8uY29tIj5GcmVlIFRlbXBsYXRlczwvYT4='); 
+	var y = document.getElementById('footer-text');
+	y.innerHTML += dt;
+	*/
+
+	/************** Tabs *********************/
+	$('ul.tabs').each(function(){
+		// For each set of tabs, we want to keep track of
+		// which tab is active and it's associated content
+		var $active, $content, $links = $(this).find('a');
+
+		// If the location.hash matches one of the links, use that as the active tab.
+		// If no match is found, use the first link as the initial active tab.
+		$active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
+		$active.addClass('active');
+
+		$content = $($active[0].hash);
+
+		// Hide the remaining content
+		$links.not($active).each(function () {
+		$(this.hash).hide();  
+		});
+
+		// Bind the click event handler
+		$(this).on('click', 'a', function(e){
+		// Make the old tab inactive.
+		$active.removeClass('active');
+		$content.hide();
+
+		// Update the variables with the new link and content
+		$active = $(this);
+		$content = $(this.hash);
+
+		// Make the tab active.
+		$active.addClass('active');
+		$content.slideToggle();
+
+		// Prevent the anchor's default click action
+		e.preventDefault();
+		});
+	});
 
 
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        items: 1,
-        dots: true,
-        loop: true,
-        nav : false
-    });
+	/************** LightBox *********************/
+	$(function(){
+		$('[data-rel="lightbox"]').lightbox();
+	});
 
 
-    // Chart Global Color
-    Chart.defaults.color = "#6C7293";
-    Chart.defaults.borderColor = "#000000";
-
-    
-})(jQuery);
+})(window, jQuery);
 
